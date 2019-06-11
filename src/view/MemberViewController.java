99c95ace37a7840b1fef0e79278a5d822a2899bd
pagliero <tore004400@gmail.com>
2019-06-11 15:55:16 +0900

    삭제 기능 추가

--- src/view/MemberViewController.java
+++ src/view/MemberViewController.java
@@ -27,6 +27,7 @@ public class MemberViewController implements Initializable {
 	@FXML	private Button btnDelete;
 	
 	@FXML	private Button btnExecute;
+	@FXML	private Button btnComso;
 	@FXML	private TextArea taExecute;
 	@FXML	private TextField tfExecute;
 	
@@ -67,10 +68,17 @@ public class MemberViewController implements Initializable {
 
 		btnCreate.setOnMouseClicked(event -> handleCreate());		
 		// btnDelete.setOnMouseClicked(e -> handleDelete());		
-		btnExecute.setOnMouseClicked(event -> handleExecute());	
-		
+		//btnExecute.setOnMouseClicked(event -> handleExecute());	
+
 		loadMemberTableView();
 	}
+	@FXML
+	private void handleComso() {
+		System.out.println("Console Comso");
+		taExecute.setText("Bless Comso");
+		
+	}
+	
 	String str = ""; // 인스턴스 변수 - 객체 변수, 객체가 존재하는 동안 메모리에 존재
 	@FXML 
 	private void handleExecute() { // event source, listener, handler
@@ -81,7 +89,7 @@ public class MemberViewController implements Initializable {
 		String name = tfExecute.getText();
 		str = str + ts.appendTextArea(name);
 		*/
-		taExecute.setText(str);
+		tfExecute.setText(str);
 	}
 	
 	private void showMemberInfo(Member member) {
@@ -138,7 +146,8 @@ public class MemberViewController implements Initializable {
 		if (selectedIndex >= 0) {
 			memberService.delete(tableViewMember.getItems().remove(selectedIndex));			
 		} else {
-			showAlert("������ �� �� �����ϴ�.");
+			showAlert("삭제를 실패하였습니다.");
+			
         }
 	}
 	
